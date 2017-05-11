630ab80d0cd9e44c373f28242cbb596b744d51cc
Vladimir Kildushev <altair194@yandex.ru>
2017-05-11 13:24:11 +0400

    add groovy start application class

---
+++ src/main/groovy/com/killprojects/Application.groovy
@@ -0,0 +1,23 @@
+package com.killprojects
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by Vladimir on 10.05.2017.
+ */
+@SpringBootApplication
+@RestController
+class Application {
+
+    @RequestMapping("/")
+    String home() {
+        "HELLO"
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args)
+    }
+}
--- src/main/java/com/killprojects/Main.kt
+++
@@ -1,6 +0,0 @@
-package com.killprojects
-
-/**
- * Created by Vladimir on 10.05.2017.
- */
-
